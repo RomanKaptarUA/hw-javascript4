2abe72a154ea3cfd0abdf04bb1dfacba03f832e9
RomanKaptarUA <romankaptar82@gmail.com>
2024-05-22 18:20:07 +0300

    ні

---
+++ index.js
@@ -0,0 +1,14 @@
+function checkFields() {
+    const fieldValue1 = document.getElementById("text-one").value;
+    const fieldValue2 = document.getElementById("text-two").value;
+    if {
+
+    } else {
+        alert("Не всі поля заповнені");
+    }
+}
+
+// 2
+function checkSum() {
+    const numberValue1
+}
