98112290dc9aa7a293ac449fd697d80ce81290e6
RomanKaptarUA <romankaptar82@gmail.com>
2024-05-22 18:46:48 +0300

    .

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-function checkFields() {
-    const fieldValue1 = document.getElementById("text-one").value;
-    const fieldValue2 = document.getElementById("text-two").value;
-    if {
-
-    } else {
-        alert("Не всі поля заповнені");
-    }
-}
+// function checkFields() {
+//     const fieldValue1 = document.getElementById("text-one").value;
+//     const fieldValue2 = document.getElementById("text-two").value;
+//     if (fieldValue1 !== '' && fieldValue2 !== ''){
+//         alert('All fields are filied');
+//     } else {
+//         alert("Не всі поля заповнені");
+//     }
+// }
 
 // 2
-function checkSum() {
-    const numberValue1
-}
+// function checkSum() {
+//     const numberValue1 = parseFloat(document.getElementById("number-one").value);
+//     const numberValue2 = parseFloat(document.getElementById("number-two").value);
+
+//     if (numberValue1 + numberValue2 > 10) {
+//         alert("Сума більша 10");
+//     } else {
+//         alert("Сума меньша або дорівнює 10");
+//     }
+// }
+// 3
+// function checkText {
+//  const inputValue = document.getElementById("textField").value;
+
+//  if (inputValue.includes("FavaScript")) {
+//     alert("Текст містить слово JavaScript")
+//  } else {
+//     alert("слово не містить слово JavaScript")
+//  }
+// }
+// 4
+// function checkNumber() {
+//     const inputValue = parseFloat(document.getElementById("numberInput").value);
+// if (isNaN(inputValue) && inputValue > 10 && inputValue < 20) {
+//     alert("Число не входить в діапазон від 10 до 20")
+// } else {
+//     alert("Число не входить в діазон від 10 до 20")
+// }
+// }
